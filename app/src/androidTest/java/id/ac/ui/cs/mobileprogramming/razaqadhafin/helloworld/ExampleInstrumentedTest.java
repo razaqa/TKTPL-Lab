0d0b2c222d1197e50fdb1235d6717a7a541dd950
razaqa <razaqa.haffiyan@amartha.com>
2020-09-21 19:55:13 +0700

    Refactor package name to id.ac.ui.cs.mobileprogramming.razaqadhafin.helloworld

--- app/src/androidTest/java/id/ac/ui/cs/mobileprogramming/razaqadhafin/helloworld/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/id/ac/ui/cs/mobileprogramming/razaqadhafin/helloworld/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.helloworld;
+package id.ac.ui.cs.mobileprogramming.razaqadhafin.helloworld;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.helloworld", appContext.getPackageName());
+        assertEquals("id.ac.ui.cs.mobileprogramming.razaqadhafin.helloworld", appContext.getPackageName());
     }
 }
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/razaqadhafin/helloworld/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/razaqadhafin/helloworld/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.helloworld;
+package id.ac.ui.cs.mobileprogramming.razaqadhafin.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
