89a9e395458f214f23cfdf7c65d2eb263ac718aa
razaqa <razaqa.haffiyan@amartha.com>
2020-09-27 02:23:57 +0700

    Refactor project name and codebase based on new submission rule

---
+++ README.md
@@ -0,0 +1,12 @@
+# TKTPL Lab
+
+Ini adalah repository yang digunakan untuk keperluan mata kuliah TKTPL 2020/2021 - Ganjil. Aplikasi yang dibuat adalah aplikasi mobile yang dijalankan di OS Android dan dibuat dengan menggunakan Android Studio.
+
+Nama 	: Razaqa Dhafin H.
+NPM		: 1706039484
+Kelas	: TKTPL - A
+
+## Lab 1
+
+Lab 1 hanya berisi empty project Android Studio dimana bisa menampilkan "Hello World" jika dijalankan di AVD.
+Deadline	: 21 September 2020 (sedikit update pada 21 September 2020 untuk nama project dll. agar lebih rapi dan karena ada perubahan codebase dari asdos)
--- app/src/androidTest/java/id/ac/ui/cs/mobileprogramming/razaqadhafin/tktplab/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/id/ac/ui/cs/mobileprogramming/razaqadhafin/tktplab/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package id.ac.ui.cs.mobileprogramming.razaqadhafin.helloworld;
+package id.ac.ui.cs.mobileprogramming.razaqadhafin.tktplab;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("id.ac.ui.cs.mobileprogramming.razaqadhafin.helloworld", appContext.getPackageName());
+        assertEquals("id.ac.ui.cs.mobileprogramming.razaqadhafin.tktplab", appContext.getPackageName());
     }
 }
