3023f7927f8163516993664b3b292ba6a25cd5c7
razaqa <razaqa.haffiyan@amartha.com>
2020-09-27 23:32:24 +0700

    Add palindrome checker feature

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/razaqadhafin/tktplab/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/razaqadhafin/tktplab/MainActivity.java
@@ -1,8 +1,10 @@
 package id.ac.ui.cs.mobileprogramming.razaqadhafin.tktplab;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void checkPalindromeButtonClicked(View view) {
+        EditText textInputPalindrome = findViewById(R.id.textInputPalindrome);
+        TextView labelResult = findViewById(R.id.labelResult);
+
+        String result = checkPalindrome(textInputPalindrome.getText().toString());
+        labelResult.setText(result);
+    }
+
+    public String checkPalindrome(String str) {
+        str = str.toLowerCase();
+        String resultPrefix = "Result : ";
+        int i = 0, j = str.length() - 1;
+
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return resultPrefix + "NOT A PALINDROME!";
+            i++;
+            j--;
+        }
+
+        return resultPrefix + "PALINDROME CONFIRMED!";
+
     }
 }
