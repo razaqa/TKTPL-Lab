94f585343f0e4039e79837e9f740fe53a962a938
razaqa <dhafins18@gmail.com>
2020-12-06 21:13:57 +0700

    Use native method uppercase in MainActivity

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/razaqadhafin/tktplab/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/razaqadhafin/tktplab/MainActivity.java
@@ -1,8 +1,11 @@
 package id.ac.ui.cs.mobileprogramming.razaqadhafin.tktplab;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.loadLibrary("uppercase-word-jni");
+    }
+
+    public native String uppercaseStringFromJNI(String input);
+
+    public void onClickUppercaseWord(View view) {
+        EditText editTextWord = findViewById(R.id.editTextWord);
+        TextView textViewOutput = findViewById(R.id.textViewOutput);
+
+        String uppercaseText = uppercaseStringFromJNI(editTextWord.getText().toString());
+        textViewOutput.setText(uppercaseText);
     }
 }
