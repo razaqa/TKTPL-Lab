a203aa224b88adfec63a38250ae1a09a866fdf5f
razaqa <dhafins18@gmail.com>
2020-12-12 22:44:07 +0700

    Add greeting functionality for lab 7

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/razaqadhafin/tktplab/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/razaqadhafin/tktplab/MainActivity.java
@@ -1,8 +1,11 @@
 package id.ac.ui.cs.mobileprogramming.razaqadhafin.tktplab;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view) {
+        EditText textInput= findViewById(R.id.editTextTextPersonName);
+        TextView labelResult = findViewById(R.id.label);
+
+        String name = textInput.getText().toString();
+        labelResult.setText("Hello, " + name + "!");
     }
 }
